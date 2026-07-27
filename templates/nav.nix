40617f3f50086f25e3167b1746a580d001ac112c
yunfachi <git@ynf.sh>
2026-07-27 22:39:35 +0000

    templates/nav: format

--- templates/nav.nix
+++ templates/nav.nix
@@ -6,8 +6,8 @@
 }:
 ''
   <p class="nav">${
-    lib.concatMapStringsSep " " (name: slib.anchor "/${lib.optionalString (name != "index") name}" "[${name}]") (
-      builtins.attrNames pages
-    )
+    lib.concatMapStringsSep " " (
+      name: slib.anchor "/${lib.optionalString (name != "index") name}" "[${name}]"
+    ) (builtins.attrNames pages)
   }</p>
 ''
