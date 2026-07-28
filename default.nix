413e6a3d0e8bb0d911c1894996eb0b1095985534
yunfachi <git@ynf.sh>
2026-07-28 05:58:34 +0000

    default: trim newlines at the end of templates

--- default.nix
+++ default.nix
@@ -1,8 +1,14 @@
-args:
+{ lib, ... }@args:
 let
   slib = import ./lib.nix (args // { inherit templates pages slib; });
 
-  templates = slib.callDir ./templates;
+  templates = lib.mapAttrs (
+    _name: value:
+    if !builtins.isFunction value then
+      lib.removeSuffix "\n" value
+    else
+      content: lib.removeSuffix "\n" (value (lib.removeSuffix "\n" content))
+  ) (slib.callDir ./templates);
   pages = slib.callDir ./pages;
 
   structure = {
