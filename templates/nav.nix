b36c33806bb66cfa3701905667a342265ef707c6
yunfachi <git@ynf.sh>
2026-08-06 18:00:04 +0300

    templates/nav: hardcode tabs

--- templates/nav.nix
+++ templates/nav.nix
@@ -8,6 +8,6 @@
   <p class="nav">${
     lib.concatMapStringsSep " " (
       name: slib.anchor "/${lib.optionalString (name != "index") name}" "[${name}]"
-    ) (builtins.attrNames pages)
+    ) ["index" "projects"]
   }</p>
 ''
