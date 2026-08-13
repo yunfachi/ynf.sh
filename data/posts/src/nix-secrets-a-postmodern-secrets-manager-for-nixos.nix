50575c64021706bad3c0bc74c99478f8ae79d370
yunfachi <git@ynf.sh>
2026-08-13 15:00:16 +0000

    data/posts/src/nix-secrets-a-postmodern-secrets-manager-for-nixos: fix typos

--- data/posts/src/nix-secrets-a-postmodern-secrets-manager-for-nixos.nix
+++ data/posts/src/nix-secrets-a-postmodern-secrets-manager-for-nixos.nix
@@ -5,19 +5,19 @@
   content = ''
     In this post, I'll quickly show you how to use the project ${slib.anchorNewTab "https://github.com/unnamed-systems/nix-secrets" "nix-secrets"} and explain its advantages over other secrets management solutions for Nix(OS), such as sops-nix, agenix, agenix-rekey, and vaultix.
 
-    First of all, nix-secrets is a NixOS module with its own CLI written in Rust (the first advantage - no blazingly fast Bash!) that lets you safely use your secrets in your configuration without leaking them.
+    First of all, nix-secrets is a NixOS module with its own CLI written in Rust (the first advantage - no blazingly fast Bash!) that lets you safely use secrets in your configuration without leaking them.
 
     > How does it work?
-    > You define the configuration for your secrets ONLY in your NixOS configuration (the second advantage - no need for a ".sops.yaml" file or standalone Nix expressions outside of your configuration, as with agenix). Then, you open an editor for the secret using our CLI. It encrypts the secret using ${slib.anchorNewTab "https://age-encryption.org/" "age"} and stores it in the directory you specify - we call this the "storage". Finally, nix-secrets decrypts and mounts the secret at the path specified in the configuration. It supports both activation scripts and systemd services when `userborn` or `systemd.sysusers` is used (the third advantage - vaultix doesn't support activation scripts, so you can't use it without `userborn` or `systemd.sysusers`).
+    > You define the configuration for your secrets ONLY in your NixOS configuration (the second advantage - no need for a ".sops.yaml" file or standalone Nix expressions outside your configuration, as with agenix). Then, you open an editor for the secret using our CLI. It encrypts the secret using ${slib.anchorNewTab "https://age-encryption.org/" "age"} and stores it in the directory you specify - we call this the "storage". Finally, nix-secrets decrypts and mounts the secret at the path specified in the configuration. It supports both activation scripts and systemd services when `userborn` or `systemd.sysusers` is used (the third advantage - vaultix doesn't support activation scripts, so you can't use it without `userborn` or `systemd.sysusers`).
 
     Advantages:
 
-    - Configuration of secrets only in your NixOS configuration - no YAML files or standalone Nix expression files.
+    - Configure secrets entirely in your NixOS configuration - no YAML files or standalone Nix expressions.
     - A CLI and activation mechanism written in Rust with atomic operations - no more slow Bash scripts.
     - Support for templates, placeholders (a feature that lets your CI use placeholders instead of real secrets), and generators.
     - Support for both systemd service activation and activation scripts.
 
-    To start using it, you need add it to your flake inputs (sorry, there is no non-flake support at the moment, but it's planed fo the future):
+    To start using it, you need to add it to your flake inputs (sorry, there is no non-flake support at the moment, but it's planned for the future):
 
     ${slib.highlightCode "nix" ''
       # flake.nix
@@ -46,18 +46,18 @@
         security.nix-secrets = {
           enable = true;
 
-          storage = ./storage; # You need to create a directory named "storage". If you use Git. don't forget to add a ".gitkeep" file to it.
+          storage = ./storage; # You need to create a directory named "storage". If you use Git, don't forget to add a ".gitkeep" file to it.
           # storagePath = "/etc/nixos/storage"; # An absolute path where your storage is located, instead of specifying it manually via the `--storage` argument.
 
           identityPaths = [
-            "/home/<user>/.config/nix-secrets/keys.txt" # You need to create this file by creating the directory `/home/<user>/.config/nix-secrets` and running `nix run github:unnamed-systems/nix-secrets#default -- keygen -o /home/<user>/.config/nix-secrets/keys.txt`
+            "/home/<user>/.config/nix-secrets/keys.txt" # You need to create this file by creating the directory `/home/<user>/.config/nix-secrets` and running `nix run github:unnamed-systems/nix-secrets#default -- keygen -o /home/<user>/.config/nix-secrets/keys.txt`.
           ];
 
           recipientAliases = {
             pc = "age17ctd...usq"; # Paste the value of "Public key" from the output of the command you used to generate the identity file.
             laptop = "age1m2...06s";
 
-            desktops = [ "pc" "laptop" ]; # Yep, you can reference aliases too
+            desktops = [ "pc" "laptop" ]; # You can reference aliases too.
           };
           defaultRecipients = [ "desktops" ]; # Add "desktops" to each secret's recipients by default.
         };
@@ -66,7 +66,7 @@
 
     At this point, you can rebuild your system to install the `nix-secrets` CLI before continuing.
 
-    Then, you need to define your first secret - for example, a user's password hash (yep, it is more secure to not publish your password hashes):
+    Then, you need to define your first secret - for example, a user's password hash (it is more secure not to publish your password hashes):
 
     ${slib.highlightCode "nix" ''
       # configuration.nix
@@ -78,7 +78,7 @@
           # mode = "0400"; # Default value; octal representation of Linux file permissions.
         };
 
-        # nix-secrets will automaticaly mount the secret's content at its `path` option.
+        # nix-secrets will automatically mount the secret's content at its `path` option.
         # You can override the value of `path` if needed.
         users.users.myUser.hashedPasswordFile = config.security.nix-secrets.secrets."user/passwordHash".path;
       }
@@ -92,7 +92,7 @@
 
     Don't forget to add the `--storage /path/to/storage` parameter if you haven't configured the `storagePath` option in your configuration (don't confuse it with the `storage` option).
 
-    Then, during a rebuild or boot, nix-secrets will decrypt the secrets from storage and mount them at their configured paths.
+    Then, during a rebuild or at boot, nix-secrets will decrypt the secrets from storage and mount them at their configured paths.
 
     You can read the full documentation at ${slib.anchorNewTab "github:unnamed-systems/nix-secrets?path=docs" "https://github.com/unnamed-systems/nix-secrets/tree/master/docs"}.
   '';
